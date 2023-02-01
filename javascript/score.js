5eeaf3298ed7fdd58546f9526f26de3ee60a6df4
Victor Vikhal <alfonso.vikhal@gmail.com>
2023-02-01 12:00:10 +0100

    mas urls modificados

---
+++ javascript/score.js
@@ -0,0 +1,12 @@
+class Score {
+    // propiedades de la puntuacion
+    constructor () {
+        this.x
+        this.y
+        this.w
+        this.h
+        this.image = new Image ()
+        this.scoreArr = ["./imagenes/40positivo.png", "./imagenes/20negativo.png", "./imagenes/2sec.png"]
+        this.image.src = this.scoreArr[]
+    }
+}
